24fe0609ff48e1a6e1bfa0feb17595efa11cef5c
RadhaDintyala <199371027+RadhaDintyala@users.noreply.github.com>
2026-01-12 12:50:24 +0530

    check logic in palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,13 @@
-n=int(input("Enter a number:"))
-while(n>0):
-    a=n%10
-    n=n//10
-    
-    if(a==n):
-        print("Palindrome")
-    else:
-        print("No")
+n = int(input("Enter a number: "))
+o = n  
+rev= 0
+
+while n > 0:
+    digit = n % 10
+    rev = (rev * 10) + digit
+    n = n // 10
+
+if o == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
